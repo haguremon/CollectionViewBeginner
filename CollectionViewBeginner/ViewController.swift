cd3e6eee95681a5bfb7ae63a60755ca894116d16
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-20 19:30:21 +0900

    tableViewのようにやってみた多分同じ知らんけど 下のCoreDataTODOAppが終わったらCollectionView beginnerやる予定

--- CollectionViewBeginner/ViewController.swift
+++ CollectionViewBeginner/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var items = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
     }
 
 
 }
-
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
+}
