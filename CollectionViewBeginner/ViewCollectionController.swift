e6acb43bfd7e8833d27ecf4ea63cd8964dc03d1a
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-22 14:49:32 +0900

    セッション毎にスクロールを変更させるやつやりたいけどわからへんサイズとか諸々わからないと難しい

--- CollectionViewBeginner/ViewCollectionController.swift
+++ CollectionViewBeginner/ViewCollectionController.swift
@@ -11,16 +11,21 @@ class ViewCollectionController: UIViewController{
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var items: [String] = ["1","2","3","4"]
+    private var items: [String] = ["1","2","3","4","5","6","7","8"]
+    let layout = UICollectionViewFlowLayout()
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
-    let layout = UICollectionViewFlowLayout()
         //topはsectionとの縦の間、leftは横の間
-//        layout.sectionInset = UIEdgeInsets(top: 30, left: 5, bottom: 10, right: .zero)
-        layout.minimumLineSpacing = 10 //セルとの縦の間
-        layout.minimumInteritemSpacing = 10 //横の間
+        collectionView.isScrollEnabled = true
+        layout.sectionInset = UIEdgeInsets(
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: .zero)
+        layout.minimumLineSpacing =  0//セルとの縦の間
+        layout.minimumInteritemSpacing = 0 //横の間
         collectionView.collectionViewLayout = layout
     }
     @IBAction func exit(segue: UIStoryboardSegue){}
@@ -32,15 +37,21 @@ class ViewCollectionController: UIViewController{
 extension ViewCollectionController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-     return 2
+     return 4
     }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch section{
         case 0:
+            layout.scrollDirection = .horizontal
             return items.count
         case 1:
+            return items.count
+        case 2:
+            layout.scrollDirection = .horizontal
+            return items.count
+        case 3:
             return items.count
         default:
             return 0
@@ -49,10 +60,47 @@ extension ViewCollectionController: UICollectionViewDelegate, UICollectionViewDa
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-        cell.layer.frame.size = CGSize(width: 100, height: 100)
+        cell.layer.frame.size = CGSize(width: 130, height: 100)
         cell.layer.cornerRadius = 20
         cell.label.text = items[indexPath.row]
-        return cell
+        //絶対もっと良い方法あるやろう↓
+        switch indexPath.row {
+        case 0:
+            cell.backgroundColor = .green
+            return cell
+        case 1:
+            cell.backgroundColor = .blue
+            return cell
+        case 2:
+            cell.backgroundColor = .brown
+            return cell
+        case 3:
+            cell.backgroundColor = .clear
+            return cell
+        case 4:
+            cell.backgroundColor = .cyan
+            return cell
+        case 5:
+            cell.backgroundColor = .darkGray
+            return cell
+        default:
+            return cell
+        }
+        //return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            return layout.scrollDirection = .horizontal
+        case 1:
+            return   layout.scrollDirection = .vertical
+        case 2:
+            return layout.scrollDirection = .horizontal
+        case 3:
+            return   layout.scrollDirection = .vertical
+        default:
+            print("")
+        }
     }
     //UICollectionViewDelegateメソッドdidSelectを認識することができるtap
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -61,7 +109,6 @@ extension ViewCollectionController: UICollectionViewDelegate, UICollectionViewDa
         vc2.collectionValue = items[indexPath.row]
         self.navigationController?.pushViewController(vc2, animated: true)
     }
-    
     
 }
 //CollectionView全体と枠との間隔を調整ができる
@@ -83,4 +130,6 @@ extension ViewCollectionController: UICollectionViewDelegateFlowLayout {
             return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         }
     }
+//collectionViewmovo
+
 }
